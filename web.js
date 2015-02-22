9538e837b3240ca117600f750b45b6370948f45a
Ubuntu <ubuntu@ip-172-31-42-120.us-west-2.compute.internal>
2015-02-22 05:36:16 +0000

    adding more

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express();
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  var fs = require('fs');
+  var buffer = fs.readFileSync('./index.html');
+  response.send(buffer.toString());
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
